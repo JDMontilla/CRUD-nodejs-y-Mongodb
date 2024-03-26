7e5055f846fc36ea7d9b3bc559ccca72d937f10a
John David Montilla <jhonmontilla14@gmail.com>
2024-03-26 07:42:40 -0500

    se crea la funcion para buscar

--- controllers/clienteController.js
+++ controllers/clienteController.js
@@ -1,9 +1,24 @@
 const Cliente = require('../models/Cliente');
 
+// Funcion para buscar clientes 
+
+exports.buscarClientes = async(req, res) => {
+    try {
+
+        const clientes = await Cliente.find();
+        res.json(clientes);
+        
+    } catch (error) {
+        console.log(error)
+        res.status(500).send('Hubo un error al agregar un cliente');
+    }
+}
+
 // Funcion agregar clientes
 exports.agregarClientes = async(req, res) => {
 
     try {
+
      let clientes;
      clientes = new Cliente(req.body)  
      await clientes.save();
