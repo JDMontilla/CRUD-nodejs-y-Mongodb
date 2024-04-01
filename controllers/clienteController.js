53ff36d2346a924a78963dbbf3958f01219b2b02
John David Montilla <jhonmontilla14@gmail.com>
2024-04-01 05:22:47 -0500

    servicios de actualizacion de datos terminado

--- controllers/clienteController.js
+++ controllers/clienteController.js
@@ -39,6 +39,7 @@ exports.buscarCliente = async(req, res) =>{
         let = cliente = await Cliente.findById(req.params.id)
         if(!cliente){
             res.status(404).json({msg: 'Cliente no encontrado con ese ID'});
+            return
         }
         res.send(cliente);
         
@@ -48,3 +49,58 @@ exports.buscarCliente = async(req, res) =>{
     }
 }
 
+// esta funcion nos sirve para eliminar un cliente
+
+exports.eliminarCliente = async(req, res) => {
+
+
+    try {
+
+        let cliente = await Cliente.findById(req.params.id);
+        if(!cliente){
+            res.status(404).json({msg: 'El cliente no existe'})
+            return
+        }
+
+        await Cliente.findOneAndDelete({_id: req.params.id}); 
+        res.json({msg: 'El cliente ha sido eliminado'});
+        return
+        
+    } catch (error) {
+        console.log(error)
+        res.status(500).send('Hubo un error al eliminar un cliente');
+    }
+}
+
+// esta funcion es para actualizar un cliente
+
+exports.actualizarCliente = async(req, res) => {
+
+    try {
+
+        const {nombres, apellidos, documento, correo, telefono, direccion} = req.body
+        let cliente = await Cliente.findById(req.params.id);
+
+        if(!cliente){
+            res.status(404).json({msg: 'El cliente no existe'})
+    
+        } else{
+            cliente.nombres = nombres;
+            cliente.apellidos = apellidos;
+            cliente.documento = documento;
+            cliente.correo = correo;
+            cliente.telefono = telefono;
+            cliente.direccion = direccion;
+
+            cliente = await Cliente.findOneAndUpdate({_id: req.params.id},  cliente,{new:true});
+            res.json(cliente);
+        }
+
+        
+    } catch (error) {
+        console.log(error)
+        res.status(500).send('Hubo un error al actualizar un cliente');
+        return
+    }
+
+}
--- routes/rutas.js
+++ routes/rutas.js
@@ -7,5 +7,7 @@ const clienteController = require('../controllers/clienteController');
 router.post('/', clienteController.agregarClientes);
 router.get('/', clienteController.buscarClientes);
 router.get('/:id', clienteController.buscarCliente);
+router.delete('/:id', clienteController.eliminarCliente);
+router.put('/:id', clienteController.actualizarCliente);
 
 module.exports = router;
